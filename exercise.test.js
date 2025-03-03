2f794fc91c161284efb401997c61d1bbe2c852d5
HKU-ANDYCHOW <chowandy@hku.hk>
2025-03-03 11:55:30 +0800

    Update exercise.test.js

--- exercise.test.js
+++ exercise.test.js
@@ -2,5 +2,6 @@ import { reverseString } from './exercise.js';
 
 test('reverseString reverses the string', () => {
     expect(reverseString('hello')).toBe('olleh');
+    expect(reverseString('word')).toBe;
     // add more tests here
 });
